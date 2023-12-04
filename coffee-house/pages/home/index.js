9aaa7f186870cd87989c84f32b288ae65684dbd2
antonina220590 <tonya2290@gmail.com>
2023-12-04 09:28:48 +0300

    feat: add burger menu functionality

--- coffee-house/pages/home/index.js
+++ coffee-house/pages/home/index.js
@@ -1,19 +1,36 @@
 //Burger menu
 
-const BURGER = document.querySelector('.hamburger__btn');
+const BURGER = document.querySelector('.hamburger');
 
 if (BURGER) {
   const NAVIGATION_DISPLAY = document.querySelector('.navigation__container');
   const NAV_LINKS = document.querySelector('.navigation__links');
-  const MENU = document.querySelector('.menu_cont');
+  const LINKS = document.querySelectorAll('.link_typography');
 
-  BURGER.addEventListener('click', () => {
+  const handleBurger = () => {
     BURGER.classList.toggle('hamburger_active');
     NAVIGATION_DISPLAY.classList.toggle('navigation__container_active');
     NAV_LINKS.classList.toggle('navigation__links_active');
-    document.body.classList.toggle("body__lock");
-  })
+    document.body.classList.toggle('body__lock');
+  }
+
+  BURGER.addEventListener('click', () => {
+    handleBurger();
+  });
+
+  LINKS.forEach((link) => {
+    link.addEventListener('click', () => {
+      console.log(link)
+      if(BURGER.classList.contains('hamburger_active')) {
+        handleBurger();
+      }
+    })
+  });
+
 };
+
+const LINKS = document.querySelectorAll('.link_typography');
+console.log(LINKS)
 
 
 

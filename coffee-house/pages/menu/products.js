9b7e5d0532ece236107c1ba92aa410258a148905
antonina220590 <tonya2290@gmail.com>
2023-12-07 19:38:03 +0300

    feat: finish categories of products part of the coffee-house-week3 task

--- coffee-house/pages/menu/products.js
+++ coffee-house/pages/menu/products.js
@@ -3,20 +3,19 @@ import productsJson from './list.js';
 const loadMoreBtn = document.querySelector('.load-more__button');
 const numberOfCards = 8;
 const numberOfCardsForTablet = 4;
-const desktopWidth = 1440;
 const tabletWidth = 768;
 
-
-let defaultCategory = '';
-let currentCategory = '';
+let defaultCategory = 'coffee';
 let currentWidth = null;
 let componentCard = null;
 let cards = [];
+let visibleCards = [];
 let cardContainer = null;
-let menuList = null;
+let menuWrapper = null;
+
 //Создаем карточки и вытягиваем данным из json
 
-function createComponent (productsJson) {
+function createComponent (productsJson) { //созданную карточку наполняем контентом из json
   if(!Array.isArray(productsJson)) {
     throw TypeError('Products array is invalid'); //проверка на массив
   }
@@ -24,26 +23,18 @@ function createComponent (productsJson) {
   componentCard.classList.add('menu__list');
 
   productsJson.forEach((product) => {
-    const cardComponent = createCard(product);
+    const cardComponent = createMenuCard(product);
     cards.push(cardComponent);
   })
+
   return componentCard;
 }
 
-function generateCards() {
-  cardContainer = createComponent(productsJson);
-  menuList = document.querySelector('.menu__list__wrapper');
-  menuList.append(cardContainer);
-  return menuList;
-}
-generateCards();
-
-function createCard (product) {
+function createMenuCard (product) { //создаем саму структуру карточки
   validateProduct(product);
 
   const component = document.createElement('div');
   component.classList.add('menu__content');
-
   const imageContainer = document.createElement('div');
   imageContainer.classList.add('menu__list__img-content');
   const image = document.createElement('img');
@@ -52,7 +43,6 @@ function createCard (product) {
   image.innerText = product.category;
 
   imageContainer.append(image);
-
 
   const infoContainer = document.createElement('div');
   infoContainer.classList.add('menu-list__text-content')
@@ -76,6 +66,7 @@ function createCard (product) {
   return component;
 }
 
+
 function validateProduct(product) {
   Object.keys(product).forEach((key) => {
     if((typeof (product[key]) ==='string' && product[key] === '') && Array.isArray(product[key]) === false) {
@@ -85,25 +76,60 @@ function validateProduct(product) {
   return true;
 }
 
-function renderCards () {
-  for (let i = 0; i < 20; i++) {
-    cardContainer.append(cards[i])
+function generateCards() { //цепляем карточки к wrapper
+
+  cardContainer = createComponent(productsJson);
+  menuWrapper = document.querySelector('.menu__list__wrapper');
+  menuWrapper.append(cardContainer);
+  return menuWrapper;
 }
+generateCards();
+
+function defineVisibleCards() { // создаем массив с карточками, которые будут отображаться в зависимости от категории
+  let visibleCards = [];
+  cards.forEach((card) => {
+    card.querySelectorAll('.menu__img').forEach(img => {
+      if(img.textContent === defaultCategory) {
+        visibleCards.push(card)
+      }
+  })
+})
+return visibleCards;
 }
 
-function changeBtnHandler () {
+function renderVisibleCards() { // отрисовываем карточки по категориям
+  componentCard.innerHTML = '';
+  visibleCards.forEach((card)=> {
+   componentCard.insertAdjacentElement("beforeend", card)
+  })
+}
+
+
+function renderPermanentCards() {
+ let coffeeBtn = document.getElementById('coffee');
+ if(coffeeBtn.classList.contains('menu__btn_active')) {
+  const activeCard = defineVisibleCards();
+      visibleCards = [...activeCard];
+      renderVisibleCards();
+ }
+}
+renderPermanentCards();
+
+function changeBtnHandler () { // перелистывание страниц
   document.querySelector('.menu__buttons').addEventListener('click', (event) => {
     if(event.target.classList.contains('menu__btn')) {
       let clickedBtn = event.target;
-      currentCategory = clickedBtn.innerText.trim().toLowerCase();
+      defaultCategory = clickedBtn.innerText.trim().toLowerCase();
       removeActiveClass();
       addActiveClass(clickedBtn);
-      renderCards();
-      filterCardsByCategory(currentCategory);
-      console.log(currentCategory)
+      const activeCard = defineVisibleCards();
+      visibleCards = [...activeCard];
+      renderVisibleCards();
+      resizeWindow();
+      btnForCategory();
     }
-  })
-}
+    });
+    }
 
 function removeActiveClass() {
   let activeBtns = document.querySelectorAll('.menu__buttons .menu__btn');
@@ -116,25 +142,17 @@ function addActiveClass(clickedBtn) {
   clickedBtn.classList.add('menu__btn_active');
 }
 
-function filterCardsByCategory(currentCategory) {
-  let visibleCards = document.querySelectorAll('.menu__list__wrapper .menu__content');
 
-  visibleCards.forEach(card => {
-    card.classList.add("menu__content_hidden");
-    card.querySelectorAll('.menu__img').forEach(img => {
-      if(img.textContent === currentCategory) {
-        card.classList.remove("menu__content_hidden");
-        console.log(currentCategory)
-      }
-    })
-    })
-}
+//Прячем и показываем лишние карточки
 
-//Test
 function hideCards() {
   if((currentWidth <= tabletWidth)) {
-     cards.slice(numberOfCardsForTablet).forEach((card) => card.classList.add('menu__content_none'))
+     visibleCards.slice(numberOfCardsForTablet).forEach((card) => card.classList.add('menu__content_none'));
   }
+ }
+
+ function showSlicedCards() {
+  visibleCards.forEach((card) => card.classList.remove('menu__content_none'));
  }
 
  // Отображение кнопки loadMore
@@ -148,14 +166,13 @@ function hideLoadBtn() {
 }
 
 function btnForCategory() {
-  if (currentCategory === 'tea') {
-    loadMoreBtn.classList.remove('load-more__button_active');
-  }
-}
-
-function showSlicedCards() {
- return cards.forEach((card) => card.classList.remove('menu__content_none'));
-
+  visibleCards.forEach((card) => {
+    card.querySelectorAll('.menu__img').forEach(img => {
+      if(img.textContent === 'tea') {
+        loadMoreBtn.classList.remove('load-more__button_active');
+      }
+  })
+})
 }
 
 // Активация кнопки загрузки
@@ -173,43 +190,28 @@ function resizeWindow() {
 
   if(currentWidth > tabletWidth) {
     if(cards.length >= numberOfCards) {
-      for (let i = 0; i < numberOfCards; i++) {
-        cardContainer.append(cards[i]);
+      showSlicedCards();
+      hideLoadBtn();
     }
+    if(cards.length <= numberOfCardsForTablet) {
+      showSlicedCards();
+      hideLoadBtn();
     }
-    if(cards.length <= numberOfCardsForTablet ) {
-      for (let i = 0; i < numberOfCardsForTablet; i++) {
-        cardContainer.append(cards[i]);
-    }
-    }
-    }
-    showSlicedCards();
-    hideLoadBtn();
+  }
 
-    if(currentWidth <= tabletWidth) {
-      if(cards.length >= numberOfCards) {
-
-        for (let i = 0; i < numberOfCards; i++) {
-                cardContainer.append(cards[i])
-            }
-      }
-
-      if (cards.length <= numberOfCardsForTablet) {
-        for (let i = 0; i < numberOfCardsForTablet; i++) {
-          cardContainer.append(cards[i])
-      }
-      }
-          hideCards();
-          showLoadBtn();
-          btnForCategory();
+  if(currentWidth <= tabletWidth) {
+    if((cards.length >= numberOfCards) || (cards.length <= numberOfCardsForTablet)) {
+      hideCards();
+      showLoadBtn();
+    }
   }
 }
 
 window.addEventListener('resize', resizeWindow)
 
 window.addEventListener('load', () => {
- //renderCards();
  changeBtnHandler();
  resizeWindow();
+
 })
 

c19956ea2e65225b4550a1c5f2372cb38b8916a4
antonina220590 <tonya2290@gmail.com>
2023-12-11 12:44:35 +0300

    feat: add autoplay, drag and stop interval functionality for slider

--- coffee-house/pages/home/slider.js
+++ coffee-house/pages/home/slider.js
@@ -8,8 +8,8 @@ let sliderWidth = SLIDE[0].clientWidth + 200;
 let offset = 0;
 let sliderCount = 0;
 
-function prevSlide() {
 
+function prevSlide() {
 sliderCount--;
 offset += sliderWidth;
 
@@ -17,35 +17,80 @@ offset += sliderWidth;
     sliderCount = 0;
     offset = 0;
   }
-
-console.log(sliderCount)
-console.log(offset)
 SLIDER_LINE.style.transform = `translateX(-${offset}px)`
 }
 
+
+
 function nextSlide() {
 
-  sliderCount++;
-  offset -= sliderWidth;
+  sliderCount--;
+  offset += sliderWidth;
 
-
-  if(sliderCount >= SLIDE.length) {
+  if(sliderCount <= -SLIDE.length) {
     offset = 0;
     sliderCount = 0;
   }
 
-  console.log(sliderCount)
-  console.log(offset)
-
-
-  SLIDER_LINE.style.transform = `translateX(${offset}px)`;
+  SLIDER_LINE.style.transform = `translateX(${-offset}px)`;
 }
-
-
 
 ARROW_LEFT.addEventListener('click', prevSlide)
 ARROW_RIGHT.addEventListener('click', nextSlide)
+let  moveInterval = null;
+
+function autoSlide() {
+moveInterval = setInterval(timer, 5000);
+  function timer() {
+    nextSlide()
+  }
+}
+
+autoSlide()
+
+// Drag and drop
+
+SLIDER_LINE.addEventListener('touchstart', touchStart, false);
+SLIDER_LINE.addEventListener('touchmove', touchMove, false);
+
+let x1 = null;
+
+function touchStart (event) {
+const initialTouch = event.touches[0];
+
+x1 = initialTouch.clientX;
+}
+
+function touchMove(event) {
+  if(!x1) {
+    return false;
+  }
+  let x2 = event.touches[0].clientX;
+  let difference = x2 - x1;
+
+  if(Math.abs(difference)) {
+    if(difference > 0){
+      prevSlide()
+    } else {
+      nextSlide()
+  }
+  x1 = 0;
+  }
+}
 
 
+//Прерывание перелистывания при наведении мыши
 
+function stopMoving() {
+  clearInterval(moveInterval);
+}
 
+function startMoving() {
+  clearInterval(moveInterval);
+  autoSlide()
+}
+
+SLIDE.forEach((slide) => {
+  slide.addEventListener('mouseover', stopMoving, true);
+  slide.addEventListener('mouseout', startMoving, true)
+})

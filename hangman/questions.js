eb13253c537f8fb9e35b8bc92830c8f4966728d0
antonina220590 <tonya2290@gmail.com>
2024-01-10 14:02:13 +0600

    feat: add js file with questions

---
+++ hangman/questions.js
@@ -0,0 +1,53 @@
+const questions = [
+  {
+    word: "everest",
+    question: "The highest mountain in the world."
+  },
+
+  {
+    word: "moon",
+    question: "What is the name of the Earth's satellite?"
+  },
+
+  {
+    word: "dog",
+    question: "Man's best friend."
+  },
+
+  {
+    word: "pride",
+    question: "What is a group of lions called?"
+  },
+
+  {
+    word: "liver",
+    question: "What is the heaviest organ in the human body?"
+  },
+
+  {
+    word: "lion",
+    question: "Which animal is known as the 'King of the Jungle'?"
+  },
+
+  {
+    word: "skin",
+    question: "What is the largest organ in the human body?"
+  },
+
+  {
+    word: "mercury",
+    question: "Which planet is closest to the Sun?"
+  },
+
+  {
+    word: "honey",
+    question: "What is the sweet substance made by bees using flower nectar?"
+  },
+
+  {
+    word: "giraffe",
+    question: "Which animal is known for its long neck?"
+  }
+];
+
+export default questions;

498790d62e96a1b8283375535533d16d5d47f1aa
antonina220590 <tonya2290@gmail.com>
2024-03-18 16:33:38 +0300

    feat: add cards to the game field and add functionality to move cards to the result block

---
+++ rss-puzzle/src/app/utils/getData.ts
@@ -0,0 +1,65 @@
+import assertValues from './assertion-functions';
+
+export default function getData(linkURL: string): void {
+    const level1RequestURL = linkURL;
+    const request = new XMLHttpRequest();
+    request.open('GET', level1RequestURL);
+    request.responseType = 'json';
+    request.send();
+
+    /* eslint-disable */
+    request.onload = function () {
+        const jsonData = request.response;
+        getWords(jsonData, 0, 0);
+    };
+}
+
+function shuffle(array: string[]): string[] {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+interface JSON {
+    rounds: [];
+    words: [];
+    textExample?: string;
+}
+
+function getWords(jsonObj: JSON, level: number, sentence: number) {
+    const levelsArr: [] = jsonObj.rounds;
+    let currentLevel: JSON = levelsArr[level];
+    let currentWords: JSON = currentLevel.words[sentence];
+    let currentSentence = currentWords.textExample;
+
+    const box = document.querySelector('.data-source');
+    assertValues(box);
+
+    const splitedSentence = currentSentence?.split(' ');
+    if (splitedSentence) {
+        shuffle(splitedSentence);
+
+        for (let i = 0; i < splitedSentence.length; i++) {
+            const div = document.createElement('div');
+            div.classList.add('word_div');
+            div.textContent = splitedSentence[i];
+            box.append(div);
+        }
+    }
+}
+
+export function changePosition(event: Event): void {
+    const target = event.target as HTMLElement;
+    const line1 = document.querySelector('.line1');
+    assertValues(line1);
+
+    if (target) {
+        if (target.classList.contains('word_div') && target.closest('.data-source')) {
+            line1.append(target);
+        }
+    }
+}
--- rss-puzzle/src/pages/gamepage/gamepage.ts
+++ rss-puzzle/src/pages/gamepage/gamepage.ts
@@ -1,6 +1,7 @@
 import './gamepage.css';
 import { CommonParams, CommonView } from '../../app/components/view';
 import { BaseElementCreator } from '../../app/components/elements-creator';
+import getData from '../../app/utils/getData';
 
 const styles = {
     GAMEWRAPPER: 'game-page__wrapper',
@@ -21,9 +22,10 @@ export default class GametView extends CommonView {
         super(params);
         this.elementCreator.setAttribute('id', 'game');
         this.showChildren();
+        this.getInfoFromGitHub();
     }
 
-    showChildren(): void {
+    showChildren(): HTMLElement {
         const divResultParams: CommonParams = {
             HTMLtag: 'div',
             classNames: [styles.RESULTBOX],
@@ -31,13 +33,16 @@ export default class GametView extends CommonView {
         const divCreator = new BaseElementCreator(divResultParams);
         const divResultTag = divCreator.getElement();
 
+        let counter = 0;
         for (let i = 0; i < numberOfLevels; i += 1) {
+            counter += 1;
             const divLineParams: CommonParams = {
                 HTMLtag: 'div',
                 classNames: [styles.LINE],
             };
             const divLineCreator = new BaseElementCreator(divLineParams);
             const divLineTag = divLineCreator.getElement();
+            divLineTag.classList.add(`line${counter}`);
             divResultTag.append(divLineTag);
         }
 
@@ -47,6 +52,15 @@ export default class GametView extends CommonView {
         };
         const divSourceCreator = new BaseElementCreator(divSourceParams);
         const divSourceTag = divSourceCreator.getElement();
+        divSourceTag.setAttribute('id', 'source');
         this.elementCreator.getElement().append(divResultTag, divSourceTag);
+
+        return divSourceTag;
+    }
+
+    getInfoFromGitHub(): void {
+        const linkURLCurrentLevel: string =
+            'https://raw.githubusercontent.com/rolling-scopes-school/rss-puzzle-data/main/data/wordCollectionLevel1.json';
+        getData(linkURLCurrentLevel);
     }
 }

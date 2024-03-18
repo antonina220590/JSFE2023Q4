1a283085d661cc9f931ade763e785e90086bb9c5
antonina220590 <tonya2290@gmail.com>
2024-03-18 21:18:29 +0300

    feat: update webpack config

--- rss-puzzle/src/app/utils/local-storage.ts
+++ rss-puzzle/src/app/utils/local-storage.ts
@@ -1,16 +1,11 @@
 import assertValues from './assertion-functions';
 
 export default class LocalStorage<T> {
-    private localStorageCheck: boolean;
-
-    constructor() {
-        this.localStorageCheck = typeof window.localStorage !== 'undefined' && window.localStorage !== null;
-    }
-
+    // private localStorageCheck: boolean;
     public setItems(key: string, value: T): void {
-        if (this.localStorageCheck) {
-            localStorage.setItem(key, JSON.stringify(value));
-        }
+        //  if (this.localStorageCheck) {
+        localStorage.setItem(key, JSON.stringify(value));
+        // }
     }
 
     getItems(key: string): T | null {
@@ -36,8 +31,6 @@ export default class LocalStorage<T> {
     }
 
     clearAll(): void {
-        if (this.localStorageCheck) {
-            localStorage.clear();
-        }
+        localStorage.clear();
     }
 }
